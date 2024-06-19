2e0490e67e6bf298629390d26c45aab490ee13d0
Carddera1842 <carddera1842@gmail.com>
2024-06-19 08:36:56 -0500

    assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -59,6 +59,7 @@ function runProgram() {
   gradeQuiz(candidateAnswers);
 }
 runProgram()
+
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
